ad1ac159d914e8e3d37e6d0b534513db12dca41a
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-16 15:29:16 -0300

    feat: add day 4 part1

---
+++ 2025/4/part1/main.rs
@@ -0,0 +1,58 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn main() {
+    let mut total: i32 = 0;
+    if let Ok(lines) = read_lines("./input") {
+        let grid: Vec<Vec<char>> = lines
+            .map_while(Result::ok)
+            .map(|line| line.chars().collect())
+            .collect();
+
+        let directions = [
+            (0, 1),
+            (1, 1),
+            (1, 0),
+            (1, -1),
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+        ];
+        for x in 0..grid.len() {
+            let row = &grid[x];
+            for y in 0..row.len() {
+                let mut streak = 0;
+                if row[y] == '@' {
+                    for (dx, dy) in directions {
+                        let x1 = (x as i32) + dx;
+                        let y1 = (y as i32) + dy;
+
+                        if x1 >= 0
+                            && y1 >= 0
+                            && (x1 as usize) < grid.len()
+                            && (y1 as usize) < row.len()
+                            && grid[x1 as usize][y1 as usize] == '@'
+                        {
+                            streak += 1;
+                        }
+                    }
+
+                    if streak < 4 {
+                        total += 1;
+                    }
+                }
+            }
+        }
+    }
+    println!("{:?}", total);
+}
